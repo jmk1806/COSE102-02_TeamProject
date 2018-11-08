af9df7cc302b408411554ceeff19fd0d8aefbaf2
unknown <young_min12@naver.com>
2018-11-08 20:54:22 +0900

    toolfinal

--- src/DrawingBoard/Tools.java
+++ src/DrawingBoard/Tools.java
@@ -1,4 +1,5 @@
 package DrawingBoard;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -7,7 +8,7 @@ import java.awt.geom.*;
 
 public class Tools extends JFrame {
 	public Tools() {
-		super("그림판 거지같은거어ㅓㅓ");
+		setTitle("Paint");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		Container container = getContentPane();
@@ -17,7 +18,7 @@ public class Tools extends JFrame {
 		container.add(workingArea, BorderLayout.CENTER);
 		container.add(menu, BorderLayout.NORTH);
 
-		addKeyListener(new KeyAdapterColor());
+		addKeyListener(new ColorHandler());
 
 		setFocusable(true);
 		requestFocus();
@@ -25,54 +26,55 @@ public class Tools extends JFrame {
 		setSize(1000, 700);
 		setVisible(true);
 	}
-	class KeyAdapterColor extends KeyAdapter {
+
+	class ColorHandler extends KeyAdapter {
 		public void keyPressed(KeyEvent e) {
 			switch (e.getKeyChar()) {
 
 			case '1':
-				BrushPanel.selectedColor=Color.RED;
+				BrushPanel.selectedColor = Color.RED;
 				break;
 
 			case '2':
-				BrushPanel.selectedColor=Color.ORANGE;
+				BrushPanel.selectedColor = Color.ORANGE;
 				break;
 
 			case '3':
-				BrushPanel.selectedColor=Color.YELLOW;
+				BrushPanel.selectedColor = Color.YELLOW;
 				break;
 
 			case '4':
-				BrushPanel.selectedColor=Color.GREEN;
+				BrushPanel.selectedColor = Color.GREEN;
 				break;
 
 			case '5':
-				BrushPanel.selectedColor=Color.CYAN;
+				BrushPanel.selectedColor = Color.CYAN;
 				break;
 
 			case '6':
-				BrushPanel.selectedColor=Color.BLUE;
+				BrushPanel.selectedColor = Color.BLUE;
 				break;
 
 			case '7':
-				BrushPanel.selectedColor=Color.MAGENTA;
+				BrushPanel.selectedColor = Color.MAGENTA;
 				break;
 
 			case '8':
-				BrushPanel.selectedColor=Color.PINK;
+				BrushPanel.selectedColor = Color.PINK;
 				break;
 
 			case '9':
-				BrushPanel.selectedColor=Color.LIGHT_GRAY;
+				BrushPanel.selectedColor = Color.LIGHT_GRAY;
 				break;
 
 			case '0':
-				BrushPanel.selectedColor=Color.GRAY;
+				BrushPanel.selectedColor = Color.GRAY;
 				break;
 			case '-':
-				BrushPanel.selectedColor=Color.DARK_GRAY;
+				BrushPanel.selectedColor = Color.DARK_GRAY;
 				break;
 			case '=':
-				BrushPanel.selectedColor=Color.BLACK;
+				BrushPanel.selectedColor = Color.BLACK;
 				break;
 			}
 
@@ -87,11 +89,11 @@ public class Tools extends JFrame {
 
 class BrushPanel extends JPanel {
 	Vector<Point> pointList = new Vector<Point>();
-	static int thickness = 1, size=1;
+	static int thickness = 1, size = 1;
 
 	static Color selectedColor;
-	Graphics g;
-	Graphics2D g2;
+	Graphics graphics;
+	Graphics2D graphics2d;
 
 	public BrushPanel() {
 		setBackground(Color.WHITE);
@@ -113,90 +115,94 @@ class BrushPanel extends JPanel {
 		}
 
 		public void mouseDragged(MouseEvent e) {
-			g=getGraphics();
-			g2=(Graphics2D) g;
-			
+			graphics = getGraphics();
+			graphics2d = (Graphics2D) graphics;
+
 			Point newPoint = e.getPoint();
 			pointList.add(newPoint);
 
 			Point start = pointList.elementAt(pointList.size() - 2);
 			Point end = pointList.elementAt(pointList.size() - 1);
-			
-			g.setColor(selectedColor);
-			g2.setStroke(new BasicStroke(thickness, BasicStroke.CAP_ROUND, 0));
-			g2.draw(new Line2D.Double(start.getX(), start.getY(), end.getX(), end.getY()));
+
+			graphics.setColor(selectedColor);
+			graphics2d.setStroke(new BasicStroke(thickness, BasicStroke.CAP_ROUND, 0));
+			graphics2d.draw(new Line2D.Double(start.getX(), start.getY(), end.getX(), end.getY()));
 		}
+
 		public void mouseClicked(MouseEvent e) {
-			g=getGraphics();
-			
-			g.fillOval(e.getX(), e.getY(), size, size);
+			graphics = getGraphics();
+
+			graphics.fillOval(e.getX(), e.getY(), size, size);
 		}
 	}
 
 }
 
 class MenuPanel extends JPanel {
-	public MenuPanel(Tools p) {
+	public MenuPanel(Tools tools) {
 		setBackground(Color.GRAY);
-		JButton chosenColor=new JButton(" ");
+		JButton chosenColor = new JButton(" ");
 		JButton brushButton = new JButton("Brush");
 		JButton eraserButton = new JButton("Eraser");
 		JButton boldButton = new JButton("Light");
-		
+
 		add(chosenColor);
 		add(brushButton);
 		add(eraserButton);
 		add(boldButton);
-		
-		brushButton.addActionListener(new BrushHandler(p));
+
+		brushButton.addActionListener(new BrushHandler(tools));
 		eraserButton.addActionListener(new EraserHandler());
-		boldButton.addActionListener(new ThicknessHandler(p));
-		chosenColor.setBackground(BrushPanel.selectedColor);
+		boldButton.addActionListener(new ThicknessHandler(tools));
 	}
-	
-	class BrushHandler implements ActionListener{
+
+	class BrushHandler implements ActionListener {
 		Tools tools;
+
 		public BrushHandler(Tools tools) {
-			this.tools=tools;
+			this.tools = tools;
 		}
+
 		public void actionPerformed(ActionEvent e) {
-			BrushPanel.selectedColor=Color.BLACK;
+			BrushPanel.selectedColor = Color.BLACK;
 			tools.requestFocus();
 		}
 	}
-	
-	class EraserHandler implements ActionListener{
+
+	class EraserHandler implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			BrushPanel.selectedColor=Color.WHITE;
+			BrushPanel.selectedColor = Color.WHITE;
 		}
 	}
 
 	class ThicknessHandler implements ActionListener {
 		Tools tools;
+
 		public ThicknessHandler(Tools tools) {
-			this.tools=tools;
+			this.tools = tools;
 		}
+
 		public void actionPerformed(ActionEvent e) {
-			JButton g = (JButton) e.getSource();
-			if (g.getText().equals("Light")) {
+			JButton button = (JButton) e.getSource();
+			if (button.getText().equals("Light")) {
 				BrushPanel.thickness = 5;
-				BrushPanel.size=3;
-				g.setText("Medium");
-				
-			} else if (g.getText().equals("Medium")) {
+				BrushPanel.size = 3;
+				button.setText("Medium");
+
+			} else if (button.getText().equals("Medium")) {
 				BrushPanel.thickness = 10;
-				BrushPanel.size=5;
-				g.setText("Bold");
-				
-			} else if (g.getText().equals("Bold")) {
+				BrushPanel.size = 5;
+				button.setText("Bold");
+
+			} else if (button.getText().equals("Bold")) {
 				BrushPanel.thickness = 20;
-				BrushPanel.size=10;
-				g.setText("Extra");
-				
-			} else if (g.getText().equals("Extra")) {
+				BrushPanel.size = 10;
+				button.setText("Extra");
+
+			} else if (button.getText().equals("Extra")) {
 				BrushPanel.thickness = 1;
-				BrushPanel.size=1;
-				g.setText("Light");
+				BrushPanel.size = 1;
+				button.setText("Light");
 			}
 			tools.requestFocus();
 		}
